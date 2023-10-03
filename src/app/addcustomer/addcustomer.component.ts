630a348cc12442d542697255373b3411c95a7fb7
Swetha S <swethassivasankaran@gmail.com>
2023-10-03 20:39:03 +0530

    Initial commit

---
+++ src/app/addcustomer/addcustomer.component.ts
@@ -0,0 +1,37 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms'
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../service/auth.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-addcustomer',
+  templateUrl: './addcustomer.component.html',
+  styleUrls: ['./addcustomer.component.css']
+})
+export class AddcustomerComponent implements OnInit {
+
+  constructor(private builder: FormBuilder, private service: AuthService, private toastr: ToastrService,
+     @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    this.service.getuserrole().subscribe(res => {
+      this.rolelist = res;
+    });
+  }
+  ngOnInit(): void {
+  }
+  rolelist: any;
+
+  registerform = this.builder.group({
+    id: this.builder.control(''),
+    name: this.builder.control(''),
+    password: this.builder.control(''),
+    email: this.builder.control(''),
+    gender: this.builder.control('male'),
+    role: this.builder.control('', Validators.required),
+    isactive: this.builder.control(false)
+  });
+  AddCustomer(){
+
+  }
+}
---
+++ src/app/deletecustomer/deletecustomer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deletecustomer',
+  templateUrl: './deletecustomer.component.html',
+  styleUrls: ['./deletecustomer.component.css']
+})
+export class DeletecustomerComponent {
+
+}
